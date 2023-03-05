79fd1801b5cbb1b47749397cc27659a626e08c90
Rodrigo Addor <me@rodrick.dev>
2023-03-05 13:48:55 -0300

    feat: home design

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import '@fontsource/jetbrains-mono';
+	import 'normalize.css';
+</script>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="caret"><span class="display">rodr</span>.dev</h1>
+
+<style lang="sass">
+  :global(body)
+    display: grid
+    place-items: center
+    width: 100vw
+    height: 100vh
+    background: #263238
+    color: #FAFAFA
+
+    *:focus-visible
+      outline: none
+  
+  h1
+    font-family: "Jetbrains Mono", monospace
+    font-size: min(4rem, 10vmin)
+  
+  .display
+    font-size: 2em
+
+  @keyframes blinking-caret
+    to
+      visibility: hidden
+
+  .caret
+    position: relative
+  
+    &::after
+      content: ""
+      position: absolute
+      display: inline-block
+      background: currentColor
+      bottom: .1em
+      width: .05em
+      height: 1.32em
+      animation: blinking-caret 1s steps(2, start) infinite
+</style>
